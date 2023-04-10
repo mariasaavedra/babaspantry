311979e10c0f0e3a21d1c16f2ac2048c6c994e3b
Maria Saavedra <msaav3@gmail.com>
2023-04-10 11:13:52 -0500

    Added contact pages

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,9 +4,17 @@ import Text from "../Text/Text";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="h-screen w-screen text-center pt-20 bg-purple">
+      <div
+        className="relative inline h-screen w-1/2"
+        style={{
+          backgroundImage: `url("/images/BP_6.webp")`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+      <div className="relative flex  py-2 items-center  h-screen w-1/2 p-8 bg-purple">
         <Text>
-          <h1 className="font-header mt-20 text-7xl">Contact Us</h1>
+          <h1 className="font-arabic text-6xl">اتصل بنا</h1>
+          <h1 className="font-header py-2 text-9xl">Contact Us</h1>
 
           <h1 className="font-header text-3xl">Location</h1>
           <p> 1019 E 63rd St, Kansas City, MO 64110</p>
@@ -17,6 +25,11 @@ function Footer() {
 
           <h1 className="font-header text-3xl">Phone</h1>
           <p>(816) 702-0045</p>
+
+          <h1 className="mt-10 text-3xl font-header">
+            We accept catering orders via our email please give us no less than
+            48 hours notice in advance.
+          </h1>
         </Text>
       </div>
     </footer>
--- components/ScrollSection/ScrollSection.tsx
+++ components/ScrollSection/ScrollSection.tsx
@@ -6,35 +6,39 @@ import Text from "../Text/Text";
 
 function ScrollSection() {
   const sectionRef = useRef(null);
+  const slideRef = useRef(null);
   const triggerRef = useRef(null);
 
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
-    const pin = gsap.fromTo(
-      sectionRef.current,
-      {
-        translateX: 0,
-      },
-      {
-        translateX: "-300vw",
-        ease: "none",
-        duration: 1,
-        scrollTrigger: {
-          trigger: triggerRef.current,
-          start: "top top",
-          end: "2000 top",
-          scrub: 0.6,
-          pin: true,
+    if (sectionRef && sectionRef.current) {
+      const pin = gsap.fromTo(
+        sectionRef.current,
+        {
+          translateX: 0,
         },
-      }
-    );
-    return () => {
-      {
-        /* A return function for killing the animation on component unmount */
-      }
-      pin.kill();
-    };
+        {
+          translateX: "-300vw",
+          ease: "none",
+          duration: 1,
+          scrollTrigger: {
+            trigger: triggerRef.current,
+            start: "top top",
+            end: "2000 top",
+            scrub: 0.6,
+            pin: true,
+          },
+        }
+      );
+      return () => {
+        {
+          /* A return function for killing the animation on component unmount */
+        }
+        pin.kill();
+      };
+      return;
+    }
   }, []);
 
   return (
@@ -57,6 +61,7 @@ function ScrollSection() {
             ></div>
             <div className="relative flex items-center  h-screen w-1/2 p-8">
               <Text>
+                <h1 className="text-6xl py-2 text-blue font-header">مرحباً</h1>
                 <h1 className="text-9xl py-2 font-header">
                   Welcome to Baba's Pantry
                 </h1>
@@ -96,9 +101,28 @@ function ScrollSection() {
               </Text>
             </div>
           </div>
+
           {/* Section 3 */}
           <div className="scroll-section bg-purple">
-            <h3>Section 3</h3>
+            <div className="relative items-center flex h-screen w-1/2 p-8">
+              <Text>
+                <h1 className="text-6xl py-2 font-header">قائمة طعام</h1>
+                <h1 className="text-9xl py-2 font-header">Menu</h1>
+                <p className="text-lg">
+                  Hungry for a delicious feast? Click on our menu photo and
+                  explore a world of mouth-watering flavors in a new page! From
+                  savory starters to indulgent desserts, satisfy your cravings
+                  with just a click.
+                </p>
+              </Text>
+            </div>
+            <div className="relative items-center flex h-screen w-1/2 p-8">
+              <img src="/images/MENU-FRONT.png"></img>
+            </div>
+
+            <div className="relative items-center flex h-screen w-1/2 p-8">
+              <img src="/images/MENU-BACK.png"></img>
+            </div>
           </div>
           {/* Section */}
           <div className="scroll-section bg-green">
