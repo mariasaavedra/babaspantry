3f9967d8df25ed7e1fe75a5e09bd9b5652887f68
Maria Saavedra <msaav3@gmail.com>
2023-05-15 17:35:02 -0500

    chore: fix footer on mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,32 +5,32 @@ function Footer() {
   return (
     <footer id="contact" className="footer">
       <div
-        className="relative inline h-screen w-1/2 border-2 border-violet"
+        className="hidden md:block relative inline h-screen w-1/2 border-2 border-violet"
         style={{
           backgroundImage: `url("/images/BP_6.webp")`,
           backgroundSize: "cover",
           backgroundPosition: "bottom",
         }}
       ></div>
-      <div className="relative flex  py-2 items-center  h-screen w-1/2 p-8 bg-purple">
+      <div className="relative flex  py-2 items-center  h-screen md:w-1/2 md:p-8 bg-purple">
         <Text>
           <img width={150} src="/images/contact-arabic.svg"></img>
-          <h1 className="font-header py-2 text-7xl">Contact Us</h1>
-
-          <h1 className="font-header text-3xl">Location</h1>
+          <h1 className="font-header py-2 text-5xl md:text-7xl">Contact Us</h1>
+        <div></div>
+          <h1 className="font-header text-2xl md:text-3xl">Location</h1>
           <p> 1019 E 63rd St, Kansas City, MO 64110</p>
 
-          <h1 className="font-header text-3xl">Hours</h1>
+          <h1 className="font-header text-2xl md:text-3xl">Hours</h1>
           <p> Monday - Saturday 11AM–7:00PM</p>
           <p>Sunday Closed</p>
 
-          <h1 className="font-header text-3xl">Phone</h1>
+          <h1 className="font-header text-2xl md:text-3xl">Phone</h1>
           <p>(816) 702-0045</p>
 
-          <h1 className="font-header text-3xl">Email</h1>
+          <h1 className="font-header text-2xl md:text-3xl">Email</h1>
           <p>babaspantrykcmo@gmail.com</p>
 
-          <h1 className="mt-10 text-3xl font-header">
+          <h1 className="mt-4 mb-10 text-xs md:text-3xl md:font-header">
             We accept catering orders via our email please give us no less than
             48 hours notice in advance. Please list the amount of people and dishes you are interested in and we will reply with pricing and amount needed for your party.
           </h1>
