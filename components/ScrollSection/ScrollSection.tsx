a47e9128763ef0a857197d5a2d3257f6c247280b
Maria Saavedra <msaav3@gmail.com>
2025-05-10 01:38:57 -0500

    chore: refactor ScrollSection component to improve structure and GSAP integration

--- components/ScrollSection/ScrollSection.tsx
+++ components/ScrollSection/ScrollSection.tsx
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { RefObject, useRef } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import SectionBakery from "@/components/SectionBakery/SectionBakery";
 import SectionCatering from "@/components/SectionCatering/SectionCatering";
@@ -8,57 +9,72 @@ import SectionWelcome from "@/components/SectionOne/SectionOne";
 import SectionMenu from "@/components/SectionThree/SectionThree";
 import SectionPress from "@/components/SectionTwo/SectionTwo";
 
+gsap.registerPlugin(ScrollTrigger);
+
 function ScrollSection() {
   const trigger = useRef(null);
   const container = useRef(null);
-  gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
+  const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
+  
+  const sections = [
+    { component: <SectionWelcome />, id: "welcome" },
+    { component: <SectionPress />, id: "press" },
+    { component: <SectionMenu />, id: "menu" },
+    { component: <SectionBakery />, id: "bakery" },
+    { component: <SectionDelivery />, id: "delivery" },
+    { component: <SectionCatering />, id: "catering" }
+  ];
 
   useGSAP(() => {
-    if (container && container.current) {
-      gsap.fromTo(
-        container.current,
-        {
-          translateX: 0,
-        },
-        {
-          translateX: "-500vw",
-          ease: "none",
-          duration: 1,
-          scrollTrigger: {
-            trigger: trigger.current,
-            start: "top top",
-            end: "2000 top",
-            scrub: 0.6,
-            pin: true,
-          },
+    if (!container.current || !sectionRefs.current.length) return;
+    
+    const totalSections = sections.length;
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: trigger.current,
+        pin: true,
+        start: "top top",
+        end: () => `+=${(totalSections - 1) * window.innerHeight}`,
+        scrub: 1,
+        snap: {
+          snapTo: 1 / (totalSections - 1), // Snap to each section
+          duration: { min: 0.2, max: 0.3 },
+          delay: 0.1,
+          ease: "power1.inOut"
         }
-      );
-    }
-  });
+      }
+    });
+    // Add each section to the timeline
+    sections.forEach((section, index) => {
+      if (index > 0) {
+        timeline.to(container.current, {
+          x: () => -(index * window.innerWidth),
+          duration: 1,
+          ease: "none"
+        }, index);
+      }
+    });
+    // Cleanup
+    return () => timeline.kill();
+  }, []);
 
   return (
     <section className="scroll-section-outer">
-      {/* The section up act just as a wrapper. If the trigger (below) is the
-      first jsx element in the component, you get an error on route change */}
-
-      {/* The div below act just as a trigger. As the doc suggests, the trigger and 
-      the animation should alway be two separated refs */}
-
       <div ref={trigger}>
-        <div ref={container} className="scroll-section-inner">
-          {/* Intro */}
-          <SectionWelcome />
-          {/* Press */}
-          <SectionPress />
-          {/* Menu */}
-          <SectionMenu />
-          {/* Bakery */}
-          <SectionBakery />
-          {/* Delivery */}
-          <SectionDelivery />
-          {/* Catering & Custom Orders */}
-          <SectionCatering />
-          {/* <SectionOne /> */}
+        <div 
+          ref={container} 
+          className="scroll-section-inner"
+          style={{ display: 'flex', width: `${sections.length * 100}vw` }}
+        >
+          {sections.map((section, index) => (
+            <div 
+              key={section.id} 
+              ref={el => { sectionRefs.current[index] = el; }}
+              style={{ width: '100vw', flexShrink: 0 }}
+            >
+              {section.component}
+            </div>
+          ))}
         </div>
       </div>
     </section>
